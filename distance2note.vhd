754925830392867c30910b9d7cdf554d259d6624
isaac-shaw <83607986+isaac-shaw@users.noreply.github.com>
2022-05-02 14:54:10 -0700

    Update distance2note.vhd

--- distance2note.vhd
+++ distance2note.vhd
@@ -1,5 +1,7 @@
+
+   
 --Isaac's Final Project ERG3083 
---VHDL Top Level Entity
+--VHDL logic to find out what note to play based on distance
 
 
 LIBRARY IEEE;
@@ -23,19 +25,27 @@ architecture arc of distance2note is
 begin
 	process(current_distance)
 		variable ratio : integer := 4;
+        variable counter : integer;
 		variable distance_int : integer;
-		variable note_temp : std_logic_vector(4 downto 0);
+		variable note_int : integer;
 		
 	begin
 		distance_int := conv_integer(current_distance);
+        
+        if( distance_int > (14*ratio) ) then
+        	note_int := 0;
+        else
+        	counter := 0;
+            
+        	for i in 1 to 13 loop
+            	if(distance_int > (counter*ratio) and distance_int < (ratio * (counter+1))) then
+                	note_int := 0 + counter;
+           		end if;
+                counter := counter + 1;
+            end loop;
+        end if;
 		
-		for I in 0 to 12 loop
-			if (distance_int > ( I * ratio ) and distance_int < ( ( I + 1) * ratio) ) then
-				note_temp := conv_std_logic_vector(I, note'length);
-			end if;
-		end loop;
-		
-		note <= note_temp;
+		note <= conv_std_logic_vector(note_int, note'length);
 		
 	end process;
 end;
